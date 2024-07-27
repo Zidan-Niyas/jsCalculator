43a16212bd9e2e3b7a37d9f91f4b2a56a54526f5
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-27 11:13:41 +0530

    added the eventlisteners for the numbers and operators to show on the display

--- scripts.js
+++ scripts.js
@@ -1,3 +1,46 @@
+const numbers = document.querySelectorAll("#number");
+const display = document.querySelector("input");
+const operators = document.querySelectorAll("#operator");
+const clear = document.querySelector(".clear")
+const equals = document.querySelector(".equals");
+
+let operatorClicked = false;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+
+operators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        if(!operatorClicked) {
+            display.value = display.value + ' ' + e.target.value + ' ';
+            operator = e.target.value;
+        }
+        operatorClicked = true;
+    })  
+});
+
+
+numbers.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if(!operatorClicked){
+            firstNumber += e.target.value;
+            display.value = firstNumber;
+        }
+        else {
+            secondNumber +=  e.target.value;
+            display.value += e.target.value;
+        }
+    });
+});
+
+clear.addEventListener("click", () => {
+    display.value = '';
+    firstNumber = '';
+    secondNumber = '';
+    operatorClicked = false;
+});
+
+
 const add = (a, b) => {
     console.log(a + b);
 };
@@ -16,21 +59,25 @@ const divide = (a, b) => {
 
 const operate = (a, b, operator) => {
     switch(operator) {
-        case "add": 
+        case "+": 
             add(a,b);
             break;
-        case "subtract" :
+        case "-" :
             subtract(a,b);
             break;
-        case "multiply" :
+        case "*" :
             multiply(a,b);
             break;
-        case "divide" :
+        case "/" :
             divide(a,b);
             break;
         default :
-            alert("Invalid Operator");
+            console.log("Invalid Operator");
     }
 }
 
-operate(1,2,"multiply");
+equals.addEventListener("click", () => {
+    console.log(`First Number : ${firstNumber}, Second Number : ${secondNumber}`);
+    console.log(operator);
+    operate(firstNumber, secondNumber, operator);
+});
